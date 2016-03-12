99a5dbdcdda9375a5c2953ff6b146d65cf5949ba
IMMYz <yz33105@gmail.com>
2016-03-12 12:30:07 +0800

    python_core

---
+++ readTextFile.py
@@ -0,0 +1,14 @@
+#get filename
+fname = input('Enter filename:')
+print()
+
+#attempt to open file for reading
+try:
+	fobj = open(fname,'r')
+except IOError as e:
+	print('*** file open error:',e)
+else:
+	#display contents to the screen
+	for eachLine in fobj:
+		print (eachLine)
+		fobj.close
